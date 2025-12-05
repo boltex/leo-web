8dc6f877cac4a10b96ebc9f4eb2652e2344d8105
Félix <felixgraphx@gmail.com>
2025-12-05 00:07:17 -0500

    Bit more setup done with workspace

--- src/workspace.ts
+++ src/workspace.ts
@@ -1,8 +1,26 @@
+export class URI {
+    // Subset of the URI class needed in leojs for workspace operations to work in leo-web
+    // members can be added as needed
+    // handle: FileSystemHandle;
+    // resolves: string[]; // path segments with root '/' being the base FileSystemDirectoryHandle
+    // fspath: string; // full path as a string from root '/' built with resolves array
+
+}
+
 class Fs {
     // File system related properties and methods can be defined here
     readFile() {
         console.log("Fs readFile method called");
     }
+    /* TODO : list of methods needed for file system operations 
+      rename
+      readFile
+      writeFile
+      delete
+      star
+      createDirectory
+      readDirectory    
+    */
 }
 
 class Workspace {
@@ -16,6 +34,11 @@ class Workspace {
         console.log("Workspace test method called");
     }
 
+    // utility methods to support leojs workspace operations can be added here
+    makeUri(path: string): URI {
+        console.log(`Making URI for path: ${path}`);
+        return new URI();
+    }
 
 }
 
