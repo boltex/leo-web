1c7dc65779a61a671811d9a5bbdc9c3390922fcd
Félix <felixgraphx@gmail.com>
2026-04-07 01:39:08 -0400

    Add toolbar buttons structure and initial comments for implementation

--- src/toolbar-buttons.ts
+++ src/toolbar-buttons.ts
@@ -1,3 +1,32 @@
+//@+leo-ver=5-thin
+//@+node:felix.20260407013645.1: * @file src/toolbar-buttons.ts
+//@+<< imports >>
+//@+node:felix.20260407013813.1: ** << imports >>
 import { ButtonEntry } from "./types";
+//@-<< imports >>
+//@+others
+//@+node:felix.20260407013836.1: ** Toolbar Buttons
+export const toolbarButtons: ButtonEntry[] = [
 
-export const toolbarButtons: ButtonEntry[] = [];
+    // Todo, implement the buttons and their actions
+    // New
+    // Open
+    // Save
+    // Prev
+    // Next
+    // dehoist
+    // hoist
+    // promote
+    // demote
+    // extract
+    // extract name
+    // import file
+    // recent files
+    // select chapter
+    // Execute Script
+];
+//@-others
+//@@language typescript
+//@@tabwidth -4
+
+//@-leo
